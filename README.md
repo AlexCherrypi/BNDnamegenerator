18d2a2fab4d1c5b008e32b40320fc99f2e991a8a
KnallbertLp <KnallbertLp@users.noreply.github.com>
2023-05-27 19:52:02 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# nsanamegenerator
+
+[![Deploy bndnamegenerator](https://github.com/AlexCherrypi/bndnamegenerator/actions/workflows/deploy.yml/badge.svg)](https://github.com/AlexCherrypi/bndnamegenerator/actions/workflows/deploy.yml) [![CC BY-SA 4.0][cc-by-sa-shield]][cc-by-sa]
+
+You might remember the old [nsanamegenerator.com](https://web.archive.org/web/20160318190656/http://www.nsanamegenerator.com/). 
+In homage to that page I firstly created [alexcherrypi.github.io/nsanamegenerator/](https://alexcherrypi.github.io/nsanamegenerator/). 
+
+But then I really wanted to create one in the mothertongue of my girlfried, because she sometimes just doesn't get the joke.
+
+By using the underlying data of [https://github.com/hdaSprachtechnologie/odenet/](https://github.com/hdaSprachtechnologie/odenet) I created this random word generator.
+
+Similar to [alexcherrypi.github.io/nsanamegenerator/](https://alexcherrypi.github.io/nsanamegenerator/), [alexcherrypi.github.io/bndnamegenerator/](https://alexcherrypi.github.io/bndnamegenerator/) currently has two versions. The OG version that combines two nouns, in all caps and witout any space to seperate ([alexcherrypi.github.io/bndnamegenerator/og/](https://alexcherrypi.github.io/bndnamegenerator/og/)) and the main version [alexcherrypi.github.io/bndnamegenerator/](https://alexcherrypi.github.io/bndnamegenerator/).
+
+This is just a little side project of mine, so don't expect regular updates and a lot of ongoing development.
+
+But if you want to suggest any features or ideas, submit them by creating an issue. I don't have any templates set up yet (and maybe never will), but don't be scared by thaat. Just write your idea down and submit the issue. :-)
+
+
+[![CC BY-SA 4.0][cc-by-sa-image]][cc-by-sa]
+
+[cc-by-sa]: http://creativecommons.org/licenses/by-sa/4.0/
+[cc-by-sa-image]: https://licensebuttons.net/l/by-sa/4.0/88x31.png
+[cc-by-sa-shield]: https://img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg
+[bndnamegenerator](https://github.com/AlexCherrypi/bndnamegenerator/) © 2023 by [AlexCherrypi](https://github.com/AlexCherrypi/) is licensed under [CC BY-SA 4.0](http://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1)
+
---
+++ generateWords.py
@@ -0,0 +1,89 @@
+from defusedxml.minidom import parseString
+import gzip 
+import requests
+from random import randint
+import os
+
+startingDir = './website/words/'
+
+
+url = 'https://github.com/hdaSprachtechnologie/odenet/blob/master/odenet/wordnet/deWordNet.xml'
+print("Downloading from '"+ url +"'")
+download = requests.get(url).content
+print("Download from '"+ url +"' finished")
+del url
+# print("Decopressing ...")
+#xml = gzip.decompress(download).decode("utf-8")
+xml = download
+del download
+print("Parsing xml ...")
+file = parseString(xml)
+del xml
+print("Finding Lemmas ...")
+lemmas = file.getElementsByTagName('Lemma')
+del file
+words = dict()
+
+print("Finding and sorting words ...")
+for lemma in lemmas:
+    pos  = lemma.getAttribute('partOfSpeech')
+    word = lemma.getAttribute('writtenForm')
+    if not word.isdigit() and len(word) > 2:
+        words.setdefault(pos,set())
+        words[pos].add(word)
+        name = pos
+        name2 = pos
+        if ' ' in word:
+            name = name +'c'
+            name2 = name2 +'c'
+            words.setdefault(pos+'c',set()) # c for combined 
+            words[pos+'c'].add(word)
+        else: 
+            name = name +'s'
+            name2 = name2 +'s'
+            words.setdefault(pos+'s',set()) # s for single 
+            words[pos+'s'].add(word)
+
+        if '-' in word:
+            name = name +'d'
+            words.setdefault(pos+'d',set()) # d for dash
+            words[pos+'d'].add(word)
+        else: 
+            name = name +'g'
+            words.setdefault(pos+'g',set()) # l for lower 
+            words[pos+'g'].add(word)
+
+        words.setdefault(name,set()) # combined
+        words[name].add(word)
+
+        if any(char.isupper() for char in word):
+            name = name +'u'
+            name2 = name2 +'u'
+            words.setdefault(pos+'u',set()) # u for upper 
+            words[pos+'u'].add(word)
+        else: 
+            name = name +'l'
+            name2 = name2 +'l'
+            words.setdefault(pos+'l',set()) # l for lower 
+            words[pos+'l'].add(word)
+
+        words.setdefault(name,set()) # combined
+        words[name].add(word)
+        words.setdefault(name2,set()) # combined
+        words[name2].add(word)
+del lemmas
+
+print("Generating files ...")
+for  key, value in words.items():
+    os.makedirs(startingDir+key, exist_ok=True)
+    print("Generating files for key '"+key+"' in '"+os.path.abspath(startingDir+key)+"'...")
+    pos = 0
+    for word in words[key]:
+        with open(startingDir+key+'/'+str(pos)+'.txt', 'w') as f:
+            f.write(word)
+        pos = pos + 1
+    with open(startingDir+key+'/len.txt', 'w') as f:
+            f.write(str(pos))
+
+print("Finished!")
+
